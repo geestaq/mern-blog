bdbf2d9be99e9ed2b06896b750879ebac8386858
Rafał Nowakowski <gee@vp.pl>
2019-07-25 19:21:21 +0200

    Init client

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/api/posts')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ posts: res });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+            {this.state.posts.map(post => <li key={post.id}>{post.title}</li>)}
+        </ul>
+      </div>
+    );
+  }
+
+};
+
+export default App;
