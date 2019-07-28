7fe2ec73332b791f8c6fe99ad6877ad66b64cf5e
geestaq <gee@vp.pl>
2019-07-28 20:28:43 +0200

    Add single post api functionality

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -8,3 +8,16 @@ exports.getPosts = async (req, res) => {
       res.status(500).json(err);
     }
 };
+
+// get single post
+exports.getSinglePost = async (req, res) => {
+    try {
+      const post = await Post.findOne({
+        id: req.params.id
+      });
+      res.status(200).json(post);
+
+    } catch(err) {
+      res.status(500).json(err);
+    }
+};
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -5,5 +5,7 @@ const PostController = require('../controllers/post.controller');
 
 // get all posts
 router.route('/posts').get(PostController.getPosts);
+// get single post
+router.route('/post/:id').get(PostController.getSinglePost);
 
 module.exports = router;
