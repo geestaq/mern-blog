b84dd3e10a65fa383c58e679fc04e6ba3e79642a
geestaq <gee@vp.pl>
2019-07-30 17:26:54 +0200

    Change links to internal state

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './NavBar.scss';
 import MainMenu from '../../layout/MainMenu/MainMenu';
 import Logo from '../../common/Logo/Logo';
 
-const links = [
-  { path: '/', title: 'Home' },
-  { path: '/posts', title: 'Posts' },
-  { path: '/post/new', title: 'New post' },
-  { path: '/contact', title: 'Contact' },
-];
-
 class NavBar extends React.Component {
 
+  state = {
+    links: [
+      { path: '/', title: 'Home' },
+      { path: '/posts', title: 'Posts' },
+      { path: '/post/new', title: 'New post' },
+      { path: '/contact', title: 'Contact' },
+    ]
+  };
+
   render() {
+    const { links } = this.state;
+
     return (
       <nav className="navbar">
         <Logo />
